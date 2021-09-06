c47c2bf77048f6a1f96ee57e7261e047fc6de14b
Cnsrgl <sarigol.can@gmail.com>
2021-09-07 01:44:13 +0200

    Added 4 new characters

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,4 @@
 module.exports = {
-    chainWebpack: (config) => {
-      const svgRule = config.module.rule('svg');
-  
-      svgRule.uses.clear();
-  
-      svgRule
-      .use('vue-loader-v16')
-      .loader('vue-loader-v16') // or `vue-loader-v16` if you are using a preview support of Vue 3 in Vue CLI
-      .end()
-      .use('vue-loader-v16')
-      .loader('vue-loader-v16');
-  },
     devServer: {
         'port': 5500
     }
